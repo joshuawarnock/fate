70bb7d47f9a13a088dcc37f28b04db389113a58b
mugillicutty <joshua.warnock@gmail.com>
2015-10-19 14:07:24 -0700

    Added gulpfile.js with gilp tasks start, test, and bower
    start- will start and restart server with changes, test- will run custom tests when there are changes or connection, bower- will add the necessary tags and links to resources on main html file

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+/**
+ * Created on 10/19/15.
+ */
+
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var nodemon = require('gulp-nodemon');
+var wiredep = require('wiredep').stream;
+
+gulp.task('start', function() {
+  nodemon({
+    script: 'server.js'
+  })
+});
+
+gulp.task('test', function() {
+  return gulp.src('test.js', {read: false})
+      .pipe(mocha());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('*.js', ['test']);
+});
+
+gulp.task('bower', function() {
+  gulp.src('index.html')
+      .pipe(wiredep({
+        optional: 'configuration',
+        goes: 'here'
+      }))
+      .pipe(gulp.dest('public/dist/'))
+});
+
+gulp.task('default', ['start', 'watch']);
