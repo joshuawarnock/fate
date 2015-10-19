3f193b398f890196249fcb01b29b7f4dcf262b1a
mugillicutty <joshua.warnock@gmail.com>
2015-10-19 15:03:36 -0700

    Added test for / route

---
+++ test.js
@@ -0,0 +1,16 @@
+/**
+ * Created on 10/19/15.
+ */
+
+var request = require('supertest')('http://localhost:1555');
+
+describe('Express Backend Test', function() {
+
+  it('Page Status & Content Type OK.', function(done){
+    request.get('/')
+        .expect(200)
+        .expect('Content-Type', 'text/html; charset=UTF-8')
+        .end(done)
+  });
+
+});
