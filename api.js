9b45787f4df8843c2f4dd0b54f8a004ef5bfedbd
mugillicutty <joshua.warnock@gmail.com>
2015-10-21 12:35:57 -0700

    Successful member id request api setup

--- api.js
+++ api.js
@@ -5,29 +5,20 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-//var destiny = require('destiny-client')();
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://www.bungie.net/platform/destiny/2/Stats/GetMembershipIdByDisplayName/mugillicutty",
-  "method": "GET",
-  "headers": {
-    "x-api-key": "aafd5349326b4892b7d2c4f50d0ab751",
-    "cache-control": "no-cache",
-    "postman-token": "7a31fcd0-a2fd-7c43-e3a1-f75df695f613"
-  }
-};
 
 router.get('/', function(req, res, data) {
-  request('https://www.bungie.net/platform/destiny/2/Stats/GetMembershipIdByDisplayName/mugillicutty',
-  function(error, response, body) {
-    var resdata = JSON.parse(body).resdata;
-    var dataobj = {};
-    for (var i = 0; i < resdata.length; i++) {
-      dataobj[i] = resdata[i].dataobj;
+  var options = {
+    url: 'https://www.bungie.net/platform/' +
+    'destiny/2/Stats/GetMembershipIdByDisplayName/mugillicutty/',
+    headers: {
+      'X-API-Key': 'aafd5349326b4892b7d2c4f50d0ab751'
     }
-    res.send(dataobj);
+  };
+  request(options, function(error, response, body) {
+    var member = JSON.parse(body);
+    res.send(member);
   })
 });
 
 module.exports = router;
+
