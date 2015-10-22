a9a662b7bd74189f01bcaa4a4b3e9f53098c4c4c
mugillicutty <joshua.warnock@gmail.com>
2015-10-22 16:16:59 -0700

    Added customer get to controller

--- client/app/app.js
+++ client/app/app.js
@@ -6,8 +6,15 @@ var app = angular.module('traitApp', ['ngResource']);
 app.controller('traitController', function($scope, $http) {
   $http({
     method: 'GET',
-    url: 'http://localhost:1555/member/mugillicutty'
+    url: 'http://localhost:1555/member/' + $scope.search
   }).success(function(response) {
     $scope.memberid = response;
+  }).then(function($scope, $http) {
+    $http({
+      method: 'GET',
+      url: 'http://localhost:1555/customer/' + response
+    }).success(function(response) {
+      $scope.customerid = response;
+    })
   })
 });
